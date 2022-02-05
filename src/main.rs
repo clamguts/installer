e02502562f37d709015336c3f3b815ecc1b6ae5c
Jared Hulbert <jaredeh@gmail.com>
2022-02-04 20:47:51 -0800

    adding runcmd

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
+/*
+ SPDX-License-Identifier: MIT
+ Copyright (c) 2022 BitFlux, Inc.
+
+ Installer script for bitflux
+*/
+
+mod runcmd;
+use crate::runcmd::RunCmd;
 
 fn main() {
-    println!("Hello world");
+    RunCmd::new("echo \"Hello World\"").execute();
 }
